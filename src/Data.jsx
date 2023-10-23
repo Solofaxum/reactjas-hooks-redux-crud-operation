20667b4376ca829fa86e23eb615cfd6269af0e63
Solofaxum <solofaxum6@gmail.com>
2023-10-23 13:32:33 -0500

    react crud basics with reduxtool kit applied

---
+++ src/Data.jsx
@@ -0,0 +1,19 @@
+export const userList = [
+    {
+        name :"Solomon Legesse",
+        email : "teshome@gmail.com",
+        id : 1
+    }, {
+        name :"Destaye Gebre",
+        email : "abrha@yahoo,com",
+        id :2
+    },{
+        name :"teshome Legesse",
+        email : "kesete@gmail.com",
+        id : 3
+    },{
+        name :"kesetebrhan Legesse",
+        email : "solofaxum@email.com",
+        id : 4
+    },
+]
---
+++ src/components/UserReducer.jsx
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { userList } from '../Data';
+
+
+export const userSlice = createSlice({
+    name : "users",
+    initialState : userList,
+    reducers : {
+
+    }
+})
+
+export default userSlice.reducer
